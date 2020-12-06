e9a8a4dc421c482930b65d91fb30e9f99b1f776e
DylanXie123 <dylanxie123@outlook.com>
2020-12-06 18:35:09 +0800

    update to typescript

---
+++ content/blog/test_md/index.md
@@ -0,0 +1,7 @@
+---
+title: 正经人谁写日记啊
+date: now
+description: empty
+---
+
+正经人谁写日记啊！
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
+  const rootPath = `/`;
+  const isRootPath = location.pathname === rootPath;
+  let header: {};
 
   if (isRootPath) {
     header = (
@@ -25,9 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()}, Built by D.X.
       </footer>
     </div>
   )
